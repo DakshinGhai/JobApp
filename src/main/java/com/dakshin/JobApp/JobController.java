1ce59cbb4fe0bc3757ca4af382ed5b0642c90c2a
DakshinGhai <dakshinghai14@gmail.com>
2025-12-18 12:54:04 +0530

    add JobController and integrate frontend views

---
+++ src/main/java/com/dakshin/JobApp/JobController.java
@@ -0,0 +1,20 @@
+package com.dakshin.JobApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+        @RequestMapping({"/","home"})
+        public String home(){
+            return "home";
+        }
+
+        @RequestMapping("addjob")
+        public String addJob(){return "addjob";}
+
+        @RequestMapping("handleForm")
+        public String handleForm(){return "success";}
+
+}
